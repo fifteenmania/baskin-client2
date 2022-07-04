e00bad7894eef82f557a84e57a53e601dabacd6e
fifteenmania <ktthee1995@gmail.com>
2022-07-04 15:35:03 +0900

    docs: remove drink keyword

--- src/feature/main/Landing.tsx
+++ src/feature/main/Landing.tsx
@@ -6,7 +6,7 @@ function LandingHeader() {
 
 function MockDescription() {
   return <div>
-    31을 말하면 지는 술게임! 어떻게 해야 살아남을 수 있을까요?
+    31을 말하면 지는 게임! 어떻게 해야 살아남을 수 있을까요?
     <ul className="list-disc list-inside">
       <li>승률 계산기로 나에게 가장 유리한 순서와 전략을 찾아보세요.</li>
       <li>컴퓨터와 플레이하세요!</li>
